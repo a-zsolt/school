9baf5ee2e6521d8abc9907f51e39df6e14c441b0
GerzsonUwU <61117480+GerzsonUwU@users.noreply.github.com>
2022-10-10 12:31:46 +0200

    5. ora

---
+++ C#/masodfoku/masodfoku/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masodfoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vátozók létrehozása
+            double a, b, c;
+            //változók dklarálása
+            Console.WriteLine("Add meg az a változó értékét:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add meg a b változó értékét:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add meg az c változó értékét:");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            //= 0 ?
+            if(a == 0 || b ==0 || c == 0)
+            {
+                Console.WriteLine("Nem adhatsz meg olyna értéket ami =< 0-nál");
+            }
+            else
+            {
+                Console.WriteLine("a = " + a + " b = " + b + ", c = " + c);
+            }
+            Console.WriteLine("-------------");
+
+            double xfirst = -b + b - 4 * a * c;
+            double x = xfirst / 2 * 9;
+
+            double x2first = -b - b - 4 * a * c;
+            double x2 = x2first/2*9;
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("x2 = " + x2);
+            Console.WriteLine("-------------");
+
+            double eredmeny = a*x2+b*c+x;
+
+            Console.WriteLine("A végeredmény: " + eredmeny);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- C#/masodfoku/masodfoku/Properties/AssemblyInfo.cs
+++ C#/masodfoku/masodfoku/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("masodiklol")]
+[assembly: AssemblyTitle("masodfoku")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("masodiklol")]
+[assembly: AssemblyProduct("masodfoku")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c8630d37-e674-439a-a6e2-2177466ec490")]
+[assembly: Guid("b0b862bf-af25-4622-94ce-8aceba2bef1c")]
 
 // Version information for an assembly consists of the following four values:
 //
