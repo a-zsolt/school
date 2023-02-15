451cef8242f14a406f869b2fd4c9cd7319e678cd
GerzsonUwU <zsolt.andorko.111@gmail.com>
2023-02-15 10:39:09 +0100

    02-15

--- python/02-06/main.py
+++ python/02-06/main.py
@@ -14,20 +14,22 @@
 
 #5.) A másodfokú egynelet eggyüthatóit várja paraméterül, és meghatározza a gyökeket
 
+import os
 import math
 
 txt = "input.txt"
 
-def read_input_no_param():
-    with open('input.txt', 'r') as f:
+def read_input(file):
+    with open(file, 'r') as f:
         numbers = f.read()
         numbers = numbers.split(',')
         numbers = [int(i) for i in numbers]
         print(numbers)
         return numbers
 
-def read_input(file):
-    with open(file, 'r') as f:
+
+def read_input_no_param():
+    with open('input.txt', 'r') as f:
         numbers = f.read()
         numbers = numbers.split(',')
         numbers = [int(i) for i in numbers]
@@ -40,12 +42,33 @@ def elsofoku(m, b):
         print("Nincs megoldás az m paraméte rnem lehet 0")
     else:
         x = -b / m
-        print("A megadott egyenlet: f(x)=", m, "x+", b)
+        print("A megadott egyenlet: f(x)=", str(m) + "x +", b)
         print("A megoldás: x=", x)
 
 def masodfoku():
     for x in read_input(txt):
         print(math.sqrt(x))
 
-elsofoku()
-masodfoku()
+def menu():
+    print("1.) Elsőfokú egyenlet megoldó")
+    print("2.) Másodfokú egyenlet megoldó")
+    print("3.) File olvasás")
+    
+    menupont = int(input("Válassz egy menüpontot: "))
+
+    if menupont == 1:
+        os.system('cls')
+        m = int(input("Add meg az egyenlet m paraméterét: "))
+        b = int(input("Add meg az egyenlet b paraméterét: "))
+        elsofoku(m, b)
+    elif menupont == 2:
+        os.system('cls')
+        masodfoku()
+    elif menupont == 3:
+        os.system('cls')
+        read_input(txt)
+    else:
+        print("Nincs ilyen menüpont!")
+
+
+menu()
