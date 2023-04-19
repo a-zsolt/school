e80f3c0eb9e6f1f5d8b5f7ac43bd71a816adbeae
GerzsonUwU <zsolt.andorko.111@gmail.com>
2023-04-19 09:10:01 +0200

    2023-04-17-Pr-alapok

--- python/04-17/italok.py
+++ python/04-17/italok.py
@@ -19,7 +19,6 @@ class italok:
 
         return lista
 
-
     def termekek_szama(self):
         termekek = self.beolvasas()
 
@@ -29,5 +28,63 @@ class italok:
                 termekek_szorp.append(i)
         return len(termekek_szorp)
     
+    def termekek_legtobb(self):
+        termekek = self.beolvasas()
+
+        termekek_szorp = []
+        for i in termekek:
+            if isinstance(i, (int, float, complex)):
+                termekek_szorp.append(i)
+
+        return print("A legtöbb szörp",termekek[termekek_szorp.index(max(termekek_szorp)) + 2], "méghozzá", max(termekek_szorp), "literel")
+    
+    def termek_atlag_ar(self):
+        termekek = self.beolvasas()
+
+        arak = []
+        for i in range(2, len(termekek), 3):
+            if isinstance(termekek[i], (int, float)):
+                arak.append(termekek[i])
+        
+        atlag_ar = round(sum(arak) / len(arak), 1)
+        return print("Átlagos ár:", atlag_ar, "Ft")
+    
+    def keszlet_erteke(self):
+        termekek = self.beolvasas()
+
+        arak = []
+        for i in range(2, len(termekek), 3):
+            if isinstance(termekek[i], (int, float)):
+                arak.append(termekek[i])
+
+        liter = []
+        for i in range(1, len(termekek), 2):
+            if isinstance(termekek[i], (int, float)):
+                liter.append(termekek[i])
+        
+        keszlet_erteke = round(sum(arak) * sum(liter), 1)
+        return print("A készlet értéke:", round(keszlet_erteke / 1000000, 1), "millió Ft")
+    
+    def atlag_feletti_termekek(self):
+        termekek = self.beolvasas()
+
+        arak = []
+        for i in range(2, len(termekek), 3):
+            if isinstance(termekek[i], (int, float)):
+                arak.append(termekek[i])
+        
+        atlag_ar = round(sum(arak) / len(arak), 1)
+
+        feletti_termekek = []
+        for i in range(2, len(termekek), 3):
+            if isinstance(termekek[i], (int, float)):
+                if termekek[i] > atlag_ar:
+                    feletti_termekek.append(termekek[i])
+
+        return print("Átlag feletti termékek:", feletti_termekek)
 
 print("A raktáron", italok("adatok.txt").termekek_szama(), "féle szörp van.")
+italok("adatok.txt").termekek_legtobb()
+italok("adatok.txt").termek_atlag_ar()
+italok("adatok.txt").keszlet_erteke()
+italok("adatok.txt").atlag_feletti_termekek()
