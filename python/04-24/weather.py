01a89a94a9fc13cba84e732a4729f44f159cd3bf
GerzsonUwU <zsolt.andorko.111@gmail.com>
2023-04-24 15:29:40 +0200

    error handling

--- python/04-24/weather.py
+++ python/04-24/weather.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 #Public api key
 api_key = "783da1bd6901fc5682d61c95496c0ce0"
@@ -8,11 +7,21 @@ location = input("Enter the city name: ")
 response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={location}&limit=1&appid={api_key}')
 data = response.json()
 
+if not data:
+    raise ValueError("No data found. Please check the city name and try again.")
+
+
 lat = data[0]['lat']
 lon = data[0]['lon']
+
 
 response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric')
 data = response.json()
 
+if not data:
+    raise ValueError("Error while trying to get weather data. Please try again.")
 
-print(f"Currently in {location} the weather is: {data['weather'][0]['description']} // {data['main']['temp']} °C // h: {data['main']['temp_max']} °C // l: {data['main']['temp_min']} °C // humidty: {data['main']['humidity']} % // wind speed: {data['wind']['speed']} km/h")
+
+print(
+    f"Currently in {location} the weather is: {data['weather'][0]['description']} // {data['main']['temp']} °C // h: {data['main']['temp_max']} °C // l: {data['main']['temp_min']} °C // humidty: {data['main']['humidity']} % // wind speed: {data['wind']['speed']} km/h"
+    )
